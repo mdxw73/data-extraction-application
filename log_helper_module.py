6271240def2790026dcc5fd14e748c12da6d978a
Zack Obied <zackobied@macbook-air.wlan.dur.ac.uk>
2023-11-04 22:55:16 +0000

    object oriented refactoring

---
+++ log_helper_module.py
@@ -0,0 +1,125 @@
+# MacOS executable setup: chmod +x qrt_data_extraction-MacOS
+
+import pandas as pd
+import subprocess
+import streamlit as st
+import time
+from datetime import datetime
+import re
+import threading
+from queue import Queue
+
+
+class LogHelper:
+    def __init__(self, selected_log_files, selected_chart="bar"):
+        self.is_live = False
+        self.tasks = Queue()
+        # most_recent_log = max(log_files, key=os.path.getctime) # Find the most recent log file based on the timestamp in the filename
+        # LOG_FILE_PATH = most_recent_log # Set the path to your log file
+
+        self.selected_log_files = selected_log_files
+        self.selected_chart = selected_chart
+
+        self.columns = ['Timestamp', 'Log Level', 'Message']
+        self.dfs = []
+        self.placeholders = []
+
+    def plot_graph(self, index):
+        if self.selected_chart == "bar":
+            self.plot_barchart(index)
+        elif self.selected_chart == "line":
+            self.plot_linechart(index)
+
+    def plot_linechart(self, index):
+        # Ensure 'Timestamp' column is of type datetime
+        self.dfs[index]['Timestamp'] = pd.to_datetime(self.dfs[index]['Timestamp'])
+        # Set 'Timestamp' as the index in a copy of the DataFrame
+        df_copy = self.dfs[index].set_index('Timestamp').copy()
+        # Create a new DataFrame for plotting (resample data to get counts per minute)
+        plot_df = df_copy.resample('1T').size().reset_index(name='Count')
+        # Plot the graph
+        with self.placeholders[index].container():
+            st.markdown(f"**File:** *{self.selected_log_files[index]}*")
+            st.line_chart(plot_df.set_index('Timestamp'))
+
+    def plot_barchart(self, index):
+        df = self.dfs[index]
+        if 'Log Level' not in df.columns:
+            raise ValueError("DataFrame does not contain a 'Log Level' column.")
+        log_level_frequencies = df['Log Level'].value_counts()
+        with self.placeholders[index].container():
+            st.markdown(f"**File:** *{self.selected_log_files[index]}*")
+            st.bar_chart(log_level_frequencies)
+
+    def show_dataframe(self, index):
+        # Ensure 'Timestamp' column is of type datetime
+        self.dfs[index]['Timestamp'] = pd.to_datetime(self.dfs[index]['Timestamp'])
+        # Set 'Timestamp' as the index in a copy of the DataFrame
+        df_copy = self.dfs[index].set_index('Timestamp').copy()
+        with self.placeholders[index].container():
+            st.markdown(f"**File:** *{self.selected_log_files[index]}*")
+            st.dataframe(df_copy)
+        
+    def update_dataframe(self, log_entry, index):
+        # Parse the log entry and update the DataFrame accordingly
+        pattern = re.compile(r'(?P<timestamp>\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}.\d{7}|\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}.\d{9}) (?P<log_level>\w+): (?P<message>.+)')
+        match = pattern.match(log_entry)
+
+        if match:
+            timestamp = match.group('timestamp')
+            log_level = match.group('log_level')
+            message = match.group('message')
+            
+            # Update the DataFrame
+            self.dfs[index].loc[len(self.dfs[index])] = [timestamp, log_level, message]
+
+    def run_thread(self, index, log_file, stop_event):
+        try:
+            with open(log_file) as file:
+                while not stop_event.is_set():
+                    where = file.tell()
+                    lines = file.readlines()
+                    if not lines:
+                        time.sleep(1)
+                        file.seek(where)
+                    else:
+                        for line in lines:
+                            self.update_dataframe(line, index)
+                        if index not in self.tasks.queue:
+                            self.tasks.put(index)
+        except KeyboardInterrupt:
+            pass
+
+    def live_track(self, closure):
+        # Create a separate thread for each log file
+        threads = []
+        stop_events = []
+        for index, log_file in enumerate(self.selected_log_files):
+            self.dfs.append(pd.DataFrame(columns=self.columns))
+            self.placeholders.append(st.empty())
+            stop_event = threading.Event()
+            stop_events.append(stop_event)
+            thread = threading.Thread(target=self.run_thread, args=(index, log_file, stop_event,))
+            threads.append(thread)
+            thread.start()
+
+        try:
+            print("Live track started")
+            while self.is_live:
+                if self.tasks.qsize() > 0:
+                    next_task = self.tasks.get()
+                    print('Executing update {} on main thread'.format(next_task))
+                    closure(next_task)
+                else:
+                    time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            # Set stop events to stop the threads
+            for stop_event in stop_events:
+                stop_event.set()
+
+            # Wait for all threads to finish
+            for thread in threads:
+                thread.join()
+            print("Live track stopped")
--- pages/Analyse.py
+++ pages/Analyse.py
@@ -1,108 +1,19 @@
-# MacOS executable setup: chmod +x qrt_data_extraction-MacOS
-
-import pandas as pd
-import subprocess
 import streamlit as st
-import time
-import os
 from glob import glob
-from datetime import datetime
-import re
-import threading
-from queue import Queue
-
-liveTrack = False
-tasks = Queue()
+import os
+from log_helper_module import LogHelper
 
 log_directory = '.'  # Replace with your log directory
 log_files = glob(os.path.join(log_directory, 'qrt_data_extraction_analysis_*.log')) # Get a list of log files in the directory
-st.markdown(f"### Analyse Log Files")
-selected_log_files = st.multiselect("Select log files", log_files)
-
 # most_recent_log = max(log_files, key=os.path.getctime) # Find the most recent log file based on the timestamp in the filename
-# LOG_FILE_PATH = most_recent_log # Set the path to your log file
 
-columns = ['Timestamp', 'Log Level', 'Message']
-dfs = []
-placeholders = []
+st.markdown(f"### Visualise Log Files")
+selected_log_files = st.multiselect("Select log files", log_files)
+log_helper = LogHelper(selected_log_files)
+live_track_button = st.empty()
 
-def update_dataframe(log_entry, index):
-    # Parse the log entry and update the DataFrame accordingly
-    pattern = re.compile(r'(?P<timestamp>\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}.\d{7}|\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2}.\d{9}) (?P<log_level>\w+): (?P<message>.+)')
-    match = pattern.match(log_entry)
-
-    if match:
-        timestamp = match.group('timestamp')
-        log_level = match.group('log_level')
-        message = match.group('message')
-        
-        # Update the DataFrame
-        dfs[index].loc[len(dfs[index])] = [timestamp, log_level, message]
-
-def show_dataframe(index):
-    # Ensure 'Timestamp' column is of type datetime
-    dfs[index]['Timestamp'] = pd.to_datetime(dfs[index]['Timestamp'])
-    # Set 'Timestamp' as the index in a copy of the DataFrame
-    df_copy = dfs[index].set_index('Timestamp').copy()
-    with placeholders[index].container():
-        st.markdown(f"**File:** *{log_files[index]}*")
-        st.dataframe(df_copy)
-
-def run_thread(index, log_file, stop_event):
-    try:
-        with open(log_file) as file:
-            while not stop_event.is_set():
-                where = file.tell()
-                lines = file.readlines()
-                if not lines:
-                    time.sleep(1)
-                    file.seek(where)
-                else:
-                    for line in lines:
-                        update_dataframe(line, index)
-                    if index not in tasks.queue:
-                        tasks.put(index)
-    except KeyboardInterrupt:
-        pass
-
-def main():
-    # Create a separate thread for each log file
-    threads = []
-    stop_events = []
-    for index, log_file in enumerate(selected_log_files):
-        dfs.append(pd.DataFrame(columns=columns))
-        placeholders.append(st.empty())
-        stop_event = threading.Event()
-        stop_events.append(stop_event)
-        thread = threading.Thread(target=run_thread, args=(index, log_file, stop_event,))
-        threads.append(thread)
-        thread.start()
-
-    try:
-        print("Live track started")
-        while liveTrack:
-            if tasks.qsize() > 0:
-                next_task = tasks.get()
-                print('Executing update {} on main thread'.format(next_task))
-                show_dataframe(next_task)
-            else:
-                time.sleep(1)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        # Set stop events to stop the threads
-        for stop_event in stop_events:
-            stop_event.set()
-
-        # Wait for all threads to finish
-        for thread in threads:
-            thread.join()
-        print("Live track stopped")
-
-liveTrackButton = st.empty()
-
-if liveTrackButton.button("Live Track", disabled=len(selected_log_files)==0):
-    if liveTrackButton.button("Stop Live Track"):
-        liveTrack = False
-    liveTrack = True
-    main()
+if live_track_button.button("Live Track", disabled=len(selected_log_files)==0):
+    if live_track_button.button("Stop Live Track"):
+        log_helper.is_live = False
+    log_helper.is_live = True
+    log_helper.live_track(log_helper.show_dataframe)
